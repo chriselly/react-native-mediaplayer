0426a8c6cf681741194f9e9934453cbe0b9743c2
chriselly <chris.elly@me.com>
2015-08-03 22:21:37 -0400

    Removed logging

--- RNMediaPlayer.m
+++ RNMediaPlayer.m
@@ -15,7 +15,7 @@
 {
   AVPlayer *_player;
   AVPlayerViewController *_playerViewcontroller;
-  
+
   NSString *_uri;
 }
 
@@ -29,22 +29,21 @@ RCT_EXPORT_METHOD(open:(NSDictionary *)options)
   // uri: STRING (full resource name with file extension)
   //
   // missing: option to disable autoplay
-  
+
   _uri = [options objectForKey:@"uri"];
-  
-  RCTLogInfo(@"Loading media: %@", _uri);
+
   NSString* mediaFilePath = [[NSBundle mainBundle] pathForResource:_uri ofType:nil];
   NSAssert(mediaFilePath, @"Media not found: %@", _uri);
-  
+
   // refactor: implement an option to load network asset instead
   NSURL *fileURL = [NSURL fileURLWithPath:mediaFilePath];
-  
+
   dispatch_async(dispatch_get_main_queue(), ^{
-  
+
     AVPlayerViewController *playerViewController = [[AVPlayerViewController alloc] init];
-    
+
     playerViewController.player = [AVPlayer playerWithURL:fileURL];
-    
+
     // autoplay
     [playerViewController.player play];
 
@@ -54,9 +53,9 @@ RCT_EXPORT_METHOD(open:(NSDictionary *)options)
     UIView *view = [ctrl view];
 
     view.window.windowLevel = UIWindowLevelStatusBar;
-    
+
     [ctrl presentViewController:playerViewController animated:TRUE completion: nil];
-    
+
   });
 }
 
